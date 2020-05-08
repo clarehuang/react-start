c579000ebf98b632e69ac9fbddc15e51cfcaa448
Clare Huang <clarehuang1126@gmail.com>
2020-05-07 22:20:29 -0700

    add ajax

--- src/client/utils/ajax.ts
+++ src/client/utils/ajax.ts
@@ -1,4 +1,22 @@
-const ajax = ({ method, url, data, success, fail, complete, dataType = 'json' }: any): void => {
+interface AjaxProps {
+  method?: string
+  url?: string
+  data?: { username: string; password: string }
+  success?: (res?: object, status?: number, xhr?: XMLHttpRequest) => void
+  fail?: (res?: object, status?: number, xhr?: XMLHttpRequest) => void
+  complete?: (res?: object, status?: number, xhr?: XMLHttpRequest) => void
+  dataType?: string
+}
+
+const ajax = ({
+  method,
+  url,
+  data,
+  success,
+  fail,
+  complete,
+  dataType = 'json',
+}: AjaxProps): void => {
   const xhr = new XMLHttpRequest()
   xhr.open(method, url)
   xhr.setRequestHeader('Content-Type', 'application/json')
@@ -15,6 +33,7 @@ const ajax = ({ method, url, data, success, fail, complete, dataType = 'json' }:
       fail(responseObj, status, xhr)
     }
     if (complete) {
+      console.log(typeof complete())
       complete(responseObj, status, xhr)
     }
   }
